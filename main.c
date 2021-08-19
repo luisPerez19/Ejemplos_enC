301104356abb1af3aa2b1c4bc179dcb13553ff04
luisperez19 <cbi2153046301@izt.uam.mx>
2021-08-18 19:18:32 -0500

    Primer commit, primer programa en C

---
+++ main.c
@@ -0,0 +1,21 @@
+/*
+Los comentarios son para nosotros del futuro
+Nombre del programa: main.c
+Autor: Luis Alfredo Pérez Mendoza
+Modificador por: Luis Alfredo Pérez Mendoza
+Descripción: Programa que imprime en consola la frase "Hola mundo"
+Necesita: un circuito
+Colocar una imagen en el repositorio - liga
+terminales que se utilizan, el modo
+ejemplo: gpio5 5_salida
+*/
+
+/* bibliotecas */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    printf("Hola mundoooo");
+
+    return 0;
+}
